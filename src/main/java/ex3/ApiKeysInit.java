6046d7a4d278da5d05f7e8b4e563843291259791
IuriiRadomskii <uraradomsky@gmail.com>
2021-12-19 17:10:21 +0300

    add temporary property file

---
+++ src/main/java/ex3/ApiKeysInit.java
@@ -0,0 +1,33 @@
+package ex3;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ApiKeysInit {
+
+    private static String get(String propertyName) {
+        FileInputStream inputStream;
+        Properties property = new Properties();
+        try {
+            inputStream = new FileInputStream("src/main/resources/properties/api.properties");
+            property.load(inputStream);
+            return property.getProperty(propertyName);
+        } catch (IOException e) {
+            System.err.println("Property file doesn't exist");
+        }
+        return "";
+    }
+
+    public static String getApiKey() {
+        return get("key");
+    }
+
+    public static String getApiToken() {
+        return get("token");
+    }
+
+    public static String getBaseURI() {
+        return get("baseURI");
+    }
+}
