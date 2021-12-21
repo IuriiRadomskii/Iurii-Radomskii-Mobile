a00ffd793d51d5fde05ab78f86a8337a792f510b
IuriiRadomskii <uraradomsky@gmail.com>
2021-12-21 20:45:17 +0300

    delete redundant interface

--- src/main/java/ex2/setup/IPageObject.java
+++
@@ -1,9 +0,0 @@
-package ex2.setup;
-
-import org.openqa.selenium.WebElement;
-
-public interface IPageObject {
-
-    WebElement getWelement(String weName) throws NoSuchFieldException, IllegalAccessException, InstantiationException;
-
-}
